28f1564502e9ab0e6e163145387d4b9ecc385318
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-12 05:16:14 -0500

    refactor: change fileService file's name

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,11 +1,11 @@
-const transformDataService = require('../services/file/transformDataService')
+const fileService = require('../services/file/fileService')
 
 async function Test(req, res) {
     res.send("test Ok")
 }
 
 async function TransformData(req, res) {
-    await transformDataService.TransformData()
+    await fileService.TransformData()
     res.send()
 }
 
