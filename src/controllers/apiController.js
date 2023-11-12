2234bf752336ff4513b72122c491c18ad0280a9f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-12 05:08:57 -0500

    feat: add TransformData controller

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,7 +1,15 @@
+const transformDataService = require('../services/file/transformDataService')
+
 async function Test(req, res) {
     res.send("test Ok")
 }
 
+async function TransformData(req, res) {
+    await transformDataService.TransformData()
+    res.send()
+}
+
 module.exports = {
-    Test
+    Test,
+    TransformData
 }
