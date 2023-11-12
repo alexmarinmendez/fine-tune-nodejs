ed32e2c0f61fc69f0816d90472a972c10c6654db
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-12 05:09:53 -0500

    feat: add transform-data controller

--- src/routers/route.js
+++ src/routers/route.js
@@ -3,5 +3,6 @@ const apiController = require('../controllers/apiController')
 const router = express.Router()
 
 router.post('/test', apiController.Test)
+router.post('/transform-data', apiController.TransformData)
 
 module.exports = router
